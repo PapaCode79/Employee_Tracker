09d72aaaa5a3160c0ce79bfbbf62ab7433d988cf
Badara Dia <pushsoccer79@gmail.com>
2022-07-06 23:04:46 -0400

    roles updates

--- Sql/seed.sql
+++ Sql/seed.sql
@@ -6,19 +6,19 @@ VALUES	('Marketing'),
 		('Development');
 
 INSERT INTO roles (title, salary, department_id)
-VALUES  ('Developer', 75000, 1),
-		('Salesperson', 50000, 2),
-		('Engineer', 70000, 3),
-		('Manager', 100000, 2);
+VALUES  ('Developer', 85000, 1),
+		('Salesperson', 65000, 2),
+		('Engineer', 75000, 3),
+		('Manager', 120000, 2);
 
 /* Foreign Key is a reference to another table in the database */
 INSERT INTO employees (first_name, last_name, role_id, manager_id)
 VALUES	('Erich', 'Purpur', 4, NULL),
-		('Jay', 'Tilley', 4, NULL),
-		('Maggie', 'Vernon', 4, NULL),
-		('Sarah', 'Bridge', 1, 1),
+		('Badara', 'Dia', 4, NULL),
+		('Awa', 'Dia', 4, NULL),
+		('Dora', 'Bridge', 1, 1),
 		('Josh', 'Shepherd', 2, 1),
 		('Dylan', 'Fuller', 3, 2),
-		('Veronica', 'Park', 1, 2),
-		('Matt', 'deCamara', 2, 3),
-		('Leif', 'Carter', 3, 3);
+		('Veronica', 'Boggs', 1, 2),
+		('Moussa', 'Dia', 2, 3),
+		('Souleyman', 'Dia', 3, 3);
